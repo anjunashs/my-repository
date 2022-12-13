85835993cdc71b455ffb3aad2703786a1a941160
anjunashs <117129615+anjunashs@users.noreply.github.com>
2022-12-12 21:56:09 -0500

    changes for flask3 and flask4

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/templates/jinja.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def head():
+    return render_template('index.html', number1=10, number2=20)
+if __name__ == '__main__':
+    app.run(debug=True)
+    
